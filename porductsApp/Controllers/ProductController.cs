ae608bcddcbae8ca18959df9faef0c3865f0e522
ahmed mohsen <ah.mohsen75@gmail.com>
2023-10-14 17:36:39 +0300

    Product: complete CRUD operations on produtcts and authorize actions.

--- porductsApp/Controllers/ProductController.cs
+++ porductsApp/Controllers/ProductController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using porductsApp.Data;
 using porductsApp.Models;
 
@@ -7,10 +10,15 @@ namespace porductsApp.Controllers
     public class ProductController : Controller
     {
         private readonly ApplicationDbContext _context;
-        public ProductController(ApplicationDbContext context)
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public ProductController(ApplicationDbContext context , UserManager<ApplicationUser> userManager)
         {
             _context = context;
+            _userManager = userManager;
         }
+
+        [Authorize]
         public IActionResult Index()
         {
             var products = _context.Products.ToList();
@@ -18,6 +26,7 @@ namespace porductsApp.Controllers
             return View(products);
         }
 
+        [Authorize(Roles = "Admin")]
         public IActionResult Create()
         {
             var product = new Product()
@@ -26,6 +35,105 @@ namespace porductsApp.Controllers
             };
 
             return View("ProductFormViewModel" , product);
+        }
+
+        [HttpPost]
+        [AutoValidateAntiforgeryToken]
+        [Authorize(Roles = "Admin")]
+        public async Task<IActionResult> Create(Product product)
+        {
+            try
+            {
+                var user =await _userManager.FindByNameAsync(User.Identity?.Name);
+                if(user != null)
+                {
+                    product.UserId = user.Id.ToString();
+                    product.CreatedBy = User.Identity?.Name ?? string.Empty;
+                }
+                
+                if (!ModelState.IsValid)
+                {
+                    product.Catalogs = _context.Catalogs.ToList();
+                    return View("ProductFormViewModel", product);
+                }
+                _context.Products.Add(product);
+                _context.SaveChanges();
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch(Exception ex)
+            {
+                throw ex;
+            }  
+        }
+
+        [Authorize(Roles = "Admin")]
+        public IActionResult Update(Guid id)
+        {
+            var product = _context.Products.FirstOrDefault(p => p.Id == id);
+
+            if(product != null)
+             product.Catalogs = _context.Catalogs.ToList();
+
+            if (product == null)
+                return NotFound();
+
+            return View("ProductFormViewModel", product);
+        }
+
+        [HttpPost]
+        [AutoValidateAntiforgeryToken]
+        [Authorize(Roles = "Admin")]
+        public async Task<IActionResult> Update(Product model)
+        {
+            try
+            {
+                if (!ModelState.IsValid)
+                {
+                    model.Catalogs = _context.Catalogs.ToList();
+                    return View("ProductFormViewModel", model);
+                }
+
+                var user = await _userManager.FindByNameAsync(User.Identity?.Name);
+
+                var product = new Product()
+                {
+                    Name = model.Name,
+                    CreationDate = model.CreationDate,
+                    CreatedBy = model.CreatedBy,
+                    StartDate = model.StartDate,
+                    Price = model.Price,
+                    CatalogId = model.CatalogId
+                };
+
+                return RedirectToAction(nameof(Index));
+
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        [Authorize(Roles = "Admin")]
+        public async Task<IActionResult> Delete(Guid id)
+        {
+            try
+            {
+                var product = await _context.Products.FirstOrDefaultAsync(p => p.Id == id);
+
+                if(product != null)
+                {
+                    _context.Products.Remove(product);
+                    await _context.SaveChangesAsync();
+                    return RedirectToAction(nameof(Index));
+                }
+                return NotFound();
+            }
+            catch(Exception ex)
+            {
+                throw ex;
+            }
         }
     }
 }
