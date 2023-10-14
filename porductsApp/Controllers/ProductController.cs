5ca70b716694109f6b7e453feb3414f9714c5039
ahmed mohsen <ah.mohsen75@gmail.com>
2023-10-14 14:28:30 +0300

    Product: Add index view and action

---
+++ porductsApp/Controllers/ProductController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using porductsApp.Data;
+using porductsApp.Models;
+
+namespace porductsApp.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        public ProductController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            var products = _context.Products.ToList();
+
+            return View(products);
+        }
+    }
+}
